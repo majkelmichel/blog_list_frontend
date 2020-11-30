297c443af5d0fe8db19c563812094fbfb360561f
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2020-11-30 21:36:39 +0100

    Ex. 5.14 done.

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,29 +1,50 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Blog from './Blog';
 
-test('renders component', () => {
-	const blog = {
-		title: 'Test title',
-		author: 'Test Author',
-		likes: 0,
-		url: 'http://testurl.test'
-	};
+describe('<Blog />', () => {
+	let component;
 
-	const mockHandler = jest.fn();
+	beforeEach(() => {
+		const blog = {
+			title: 'Test title',
+			author: 'Test Author',
+			likes: 0,
+			url: 'http://testurl.test'
+		};
+		const mockHandler = jest.fn();
+		component = render(
+			<Blog id='test' addLike={mockHandler} loggedInId='12' removeBlog={mockHandler} userId='12' {...blog} />
+		);
+	});
 
-	const component = render(
-		<Blog id='test' addLike={mockHandler} loggedInId='12' removeBlog={mockHandler} userId='12' {...blog} />
-	);
+	test('renders component', () => {
 
-	expect(component.container).toHaveTextContent(
-		'Test title'
-	);
-	expect(component.container).toHaveTextContent(
-		'Test Author'
-	);
+		expect(component.container).toHaveTextContent(
+			'Test title'
+		);
+		expect(component.container).toHaveTextContent(
+			'Test Author'
+		);
 
-	const detailed = component.container.querySelector('.details');
-	expect(detailed).toHaveStyle('display: none;');
+		const detailed = component.container.querySelector('.details');
+		expect(detailed).toHaveStyle('display: none;');
+	});
+
+	test('shows likes and link after clicking show button', () => {
+		const button = component.getByText('view');
+		fireEvent.click(button);
+
+		expect(component.container).toHaveTextContent(
+			'likes:'
+		);
+
+		expect(component.container).toHaveTextContent(
+			'http://testurl.test'
+		);
+
+		const detailed = component.container.querySelector('.details');
+		expect(detailed).not.toHaveStyle('display: none;');
+	});
 });
